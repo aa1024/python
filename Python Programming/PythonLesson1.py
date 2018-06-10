83e458c2f1feddac8585c7c11a763fd32ee31c4c
aa1024 <11321158+aa1024@users.noreply.github.com>
2018-06-10 01:23:59 +0100

    lesson 1

---
+++ Python Programming/PythonLesson1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  9 01:00:13 2018
+
+@author: alika
+"""
+#%%
+def areatriangle(b,h):
+   area = 0.5 * b * h 
+   print("The are of a traingle )of base",b,"and height",h,"is" ,area)
+   
+    
