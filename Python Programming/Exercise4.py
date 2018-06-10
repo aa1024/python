b6ea27bef9ff7e77b2d3dacf04b71e1364e98774
aa1024 <11321158+aa1024@users.noreply.github.com>
2018-06-10 12:53:19 +0100

    input string and concatenate

--- Python Programming/Exercise4.py
+++ Python Programming/Exercise4.py
@@ -1,5 +1,7 @@
 #%%
 def name():
+    """ Ask the first name and last name, concatenate and print. 
+        Also ask the city and state and print"""
     fname = input("Enter your first name:")
     lname = input("Enter your last name:")
     city = input("Enter the city you live in:")
